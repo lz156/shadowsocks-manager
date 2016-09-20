d0722cf01f72036ce69ef22ff76fb1ea66cdf3eb
Guyuteng <igyteng@gmail.com>
2016-09-20 16:21:16 +0800

    null

--- server.js
+++ server.js
@@ -4,22 +4,15 @@ require('babel-core/register');
 const path = require('path');
 const program = require('commander');
 program
-  .version('0.0.1')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .version('0.0.4')
   .parse(process.argv);
 
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
+console.log('This version is unavailable');
+// console.log(program);
 
 module.exports = require('bamei').create(function (ctx) {
   // ctx.task(path.resolve(__dirname, './services'));
-  console.log(ctx.config.all());
+  // console.log(ctx.config.all());
 
   ctx.init(err => {
     if (err) throw err;
